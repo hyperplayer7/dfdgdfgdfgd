708ae3d42c291105ec57db4b8c39e42329f64c43
hyperplayer7 <hyperplayer7@yahoo.com>
2023-02-21 20:41:52 +0800

    a

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 5000;
-const server = http.createServer(app);
+const hostname = '127.0.0.1';
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+  });
 
-server.listen(port,  () => {
-    console.log(`started on port ${port}`);
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
